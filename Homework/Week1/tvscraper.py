7a04707a88e6b66c3027d6961dfd5e338be0dbb7
terschure <anneke.terschure@student.uva.nl>
2015-10-30 16:20:29 +0100

    Basic working scraper

--- Homework/Week1/tvscraper.py
+++ Homework/Week1/tvscraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Anneke ter Schure
+# Student number: 6084087
 '''
 This script scrapes IMDB and outputs a CSV file with highest ranking tv series.
 '''
@@ -10,7 +10,7 @@ import csv
 from pattern.web import URL, DOM
 
 TARGET_URL = "http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&start=1&title_type=tv_series"
-BACKUP_HTML = 'tvseries.html'
+BACKUP_HTML = 'tvseries.html' # local backup of IMDB address
 OUTPUT_CSV = 'tvseries.csv'
 
 
@@ -31,8 +31,46 @@ def extract_tvseries(dom):
     # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
     # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
 
-    return []  # replace this line as well as appropriate
+    # initialise
+    tvseries = []
+    test = ""
 
+    # loop over all entries on the webpage
+    for element in dom.by_tag("td.title"):
+        series = []
+
+        # get title and ranking and push to series list
+        title = element.by_tag("a")[0].content
+        series.append(title)
+        series.append(element.by_tag("span.value")[0].content)
+
+        # loop over genres to push all to series list
+        genres = ""
+        i = 0
+        for el in element.by_tag("span.genre"):
+            genre = el.by_tag("a")[i].content
+            genres += genre
+            i += 1
+        series.append(genres)
+
+        # loop over actors to push all to series list
+        actors = ""
+        j = 0
+        for el in element.by_tag("span.credit"):
+            actor = el.by_tag("a")[j].content
+            actors += actor
+            j += 1
+        series.append(actors)
+
+        # get runtime and extract the digits
+        runtime = element.by_tag("span.runtime")[0].content
+        time = int(''.join(el for el in runtime if el.isdigit())) # source: http://stackoverflow.com/questions/10365225/extract-digits-in-a-simple-way-from-a-python-string
+        series.append(time)
+
+        tvseries.append(series)
+
+    print tvseries
+    return tvseries
 
 def save_csv(f, tvseries):
     '''
@@ -40,6 +78,9 @@ def save_csv(f, tvseries):
     '''
     writer = csv.writer(f)
     writer.writerow(['Title', 'Ranking', 'Genre', 'Actors', 'Runtime'])
+
+    for i in range(50):
+        writer.writerow(tvseries[i])
 
     # ADD SOME CODE OF YOURSELF HERE TO WRITE THE TV-SERIES TO DISK
 
